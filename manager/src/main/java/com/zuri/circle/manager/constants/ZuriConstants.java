b89f5e6fa30c0aaea87b538e2c2bf6b99d161ba6
kumarkalyan93 <kkalyan3@asu.edu>
2019-10-19 15:45:14 -0700

    added models

--- manager/src/main/java/com/zuri/circle/manager/constants/ZuriConstants.java
+++ manager/src/main/java/com/zuri/circle/manager/constants/ZuriConstants.java
@@ -2,4 +2,16 @@ package com.zuri.circle.manager.constants;
 
 public class ZuriConstants {
 
+	public static final String PREF_CONTACT_EMAIL ="email";
+	public static final String PREF_CONTACT_PHONE ="phone";
+	
+	public static final String USER_TYPE_DONOR = "Donor";
+	public static final String USER_TYPE_VOLUNTEER = "Volunteer";
+	public static final String USER_TYPE_NEED_HELP ="NeedHelp";
+	
+	
+	
+	
+	
+	
 }
---
+++ manager/src/main/java/com/zuri/circle/manager/models/NeedHelp.java
@@ -0,0 +1,49 @@
+package com.zuri.circle.manager.models;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+@Document(collection = "NeedHelp")
+public class NeedHelp implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	  private String id;
+      private User user;
+      private List<String> helpType;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public List<String> getHelpType() {
+		return helpType;
+	}
+	public void setHelpType(List<String> helpType) {
+		this.helpType = helpType;
+	}
+	public NeedHelp(String id, User user, List<String> helpType) {
+		super();
+		this.id = id;
+		this.user = user;
+		this.helpType = helpType;
+	}
+      
+	public NeedHelp() {
+		
+	}
+      
+
+}
